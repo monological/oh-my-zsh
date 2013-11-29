9175897aee770888306f9f5f6b8f4c6cbb8b6fe4
Julian Lupu <monological@gmail.com>
2013-11-28 23:45:39 -0800

    +fix git aliases

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -46,38 +46,86 @@ function work_in_progress() {
 #
 
 alias g='git'
-
-alias ga='git add'
-alias gaa='git add --all'
-alias gapa='git add --patch'
-
-alias gb='git branch'
-alias gba='git branch -a'
-alias gbda='git branch --merged | command grep -vE "^(\*|\s*master\s*$)" | command xargs -n 1 git branch -d'
-alias gbl='git blame -b -w'
-alias gbnm='git branch --no-merged'
-alias gbr='git branch --remote'
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
-
-alias gc='git commit -v'
-alias gc!='git commit -v --amend'
-alias gca='git commit -v -a'
-alias gca!='git commit -v -a --amend'
-alias gcan!='git commit -v -a -s --no-edit --amend'
-alias gcb='git checkout -b'
-alias gcf='git config --list'
-alias gcl='git clone --recursive'
-alias gclean='git reset --hard && git clean -dfx'
-alias gcm='git checkout master'
+compdef g=git
+alias gs='git status'
+compdef _git gs=git-status
+alias gd='git diff'
+compdef _git gd=git-diff
+alias gdc='git diff --cached'
+compdef _git gdc=git-diff
+alias gdt='git diff-tree --no-commit-id --name-only -r'
+compdef _git gdc=git diff-tree --no-commit-id --name-only -r
+alias gl='git pull'
+compdef _git gl=git-pull
+alias gup='git pull --rebase'
+compdef _git gup=git-fetch
+alias gp='git push'
+compdef _git gp=git-push
+alias gd='git diff'
+gdv() { git diff -w "$@" | view - }
+compdef _git gdv=git-diff
+alias gm='git commit -v -m'
+compdef _git gm=git-commit
+alias gm!='git commit -v --amend'
+compdef _git gm!=git-commit
+alias gma='git commit -v -a -m'
+compdef _git gm=git-commit
+alias gma!='git commit -v -a --amend'
+compdef _git gma!=git-commit
 alias gcmsg='git commit -m'
-alias gco='git checkout'
+compdef _git gcmsg=git-commit
+alias gc='git checkout'
+compdef _git gc=git-checkout
+alias gcm='git checkout master'
+alias gr='git remote'
+compdef _git gr=git-remote
+alias grv='git remote -v'
+compdef _git grv=git-remote
+alias grmv='git remote rename'
+compdef _git grmv=git-remote
+alias grrm='git remote remove'
+compdef _git grrm=git-remote
+alias grset='git remote set-url'
+compdef _git grset=git-remote
+alias grup='git remote update'
+compdef _git grset=git-remote
+alias grbi='git rebase -i'
+compdef _git grbi=git-rebase
+alias grbc='git rebase --continue'
+compdef _git grbc=git-rebase
+alias grba='git rebase --abort'
+compdef _git grba=git-rebase
+alias gb='git branch'
+compdef _git gb=git-branch
+alias gba='git branch -a'
+compdef _git gba=git-branch
+alias gbr='git branch --remote'
 alias gcount='git shortlog -sn'
 compdef gcount=git
 alias gcp='git cherry-pick'
+compdef _git gcp=git-cherry-pick
+alias glg='git log --stat --max-count=10'
+compdef _git glg=git-log
+alias glgg='git log --graph --max-count=10'
+compdef _git glgg=git-log
+alias glgga='git log --graph --decorate --all'
+compdef _git glgga=git-log
+alias glo='git log --oneline --decorate --color'
+compdef _git glo=git-log
+alias glog='git log --oneline --decorate --color --graph'
+compdef _git glog=git-log
+alias gss='git status -s'
+compdef _git gss=git-status
+alias ga='git add'
+compdef _git ga=git-add
+#alias gm='git merge'
+#compdef _git gm=git-merge
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
+alias gclean='git reset --hard && git clean -dfx'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+
+# Sign and verify commits with GPG
 alias gcs='git commit -S'
 
 alias gd='git diff'
@@ -87,11 +135,10 @@ gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
 alias gdw='git diff --word-diff'
 
-alias gf='git fetch'
-alias gfa='git fetch --all --prune'
-function gfg() { git ls-files | grep $@ }
-compdef gfg=grep
-alias gfo='git fetch origin'
+
+#alias gpoat='git push origin --all && git push origin --tags'
+#alias gmt='git mergetool --no-prompt'
+#compdef _git gm=git-mergetool
 
 alias gg='git gui citool'
 alias gga='git gui citool --amend'
